5fe4b21cd52844a4ad2ef3c8b4e17e09dd8de0b3
evelynhelena <evelynhelena781@gmail.com>
2022-02-04 20:55:09 -0300

    criando inicio da funcionalidade do app

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,7 +1,35 @@
-export function Todo(){
-    return(
-        <>
-            <h1>ola mundo do TODO</h1>
-        </>
-    )
+import { useState } from "react";
+import { Container } from "./styles";
+export function Todo() {
+  const [tools, setTools] = useState([]);
+
+  const handleAddTools = () => {
+    const teste = 
+      {
+        id: 1,
+        titleTool: "Comprar Arroz",
+        indFinish: false,
+      }
+    ;
+    setTools((oldTools) => [...oldTools, teste]);
+  };
+
+  return (
+    <Container>
+      <div className="inputs">
+        <input type="text" placeholder="Tarefa" />
+        <button type="button" onClick={handleAddTools}>
+          Add
+        </button>
+      </div>
+      <div className="tools-box">
+        {tools.map((tool) => (
+          <div className="tools" key={tool.id}>
+            <input type="checkbox" />
+            <label>{tool.titleTool}</label>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 }
---
+++ src/components/Todo/styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  padding: 2.5rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .inputs {
+    margin-bottom: 1rem;
+    width: 600px;
+    input{
+        padding: 10px;
+        width: 500px;
+    }
+    button{
+        padding:10px;
+        width: 90px;
+        margin-left: 10px;
+        background-color: green;
+        border: 0;
+        color: #fff;
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
+  }
+
+  .tools-box {
+    width: 600px;
+    max-height: 100;
+    overflow-y: auto;
+    padding: 15px;
+    background-color: #eee;
+
+    .tools{
+        border-bottom: 1px solid #ddd;
+        padding: 10px 0;
+        margin-bottom: 15px;
+        input{
+            margin-right: 5px;
+        }
+    }
+  }
+`;
