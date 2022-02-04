d84a7e5ea0fe2aafcb7a876f664f65c750afa737
evelynhelena <evelynhelena781@gmail.com>
2022-02-03 21:28:13 -0300

    iniciando

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Todo } from "./components/Todo";
+import GlobalStyle from "./styles/global";
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Todo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo/index.jsx
@@ -0,0 +1,7 @@
+export function Todo(){
+    return(
+        <>
+            <h1>ola mundo do TODO</h1>
+        </>
+    )
+}
